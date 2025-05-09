db2f9276ebcc01698d1a781049436a23d2841a81
Pratyusha Musunuru <pratyusha792@gmail.com>
2025-05-09 18:14:46 +0100

    longest_substring_no_repeat

---
+++ Strings/long_sub_no_repeat.py
@@ -0,0 +1,21 @@
+#problem: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+#used sliding window & 2 pointers approaches
+s = "abcabcbb"
+if s == "":
+    return 0
+right = 0
+left = 0
+l = len(s)
+a = {}
+ans = 1
+prev_len = 0
+while right < l:
+    if a.get(s[right], -1) >= left:
+        left = max(a[s[right]]+1, left)
+        prev_len = right - left + 1
+    else:
+        prev_len += 1
+        ans = max(ans, prev_len)
+    a[s[right]] = right
+    right += 1
+print(ans)
