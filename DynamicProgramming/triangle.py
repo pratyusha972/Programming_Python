e3a1a3cf1c40cce10475bdd95e9919d32439f2f8
Pratyusha Musunuru <pratyusha792@gmail.com>
2025-05-30 00:45:33 +0100

    update triangle

--- DynamicProgramming/triangle.py
+++ DynamicProgramming/triangle.py
@@ -1,11 +1,8 @@
 #Problem:https://leetcode.com/problems/triangle/
 triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
 for i in range(1, len(triangle)):
-    for j in range(0, len(triangle[i])):
-        if j == 0:
-            triangle[i][j] += triangle[i-1][0]
-        elif j == len(triangle[i])-1:
-            triangle[i][j] += triangle[i-1][-1]
-        else:
-            triangle[i][j] += min(triangle[i-1][j], triangle[i-1][j-1])
+    for j in range(1, len(triangle[i])-1):
+        triangle[i][j] += min(triangle[i-1][j], triangle[i-1][j-1])
+    triangle[i][0] += triangle[i-1][0]
+    triangle[i][len(triangle[i])-1] += triangle[i-1][-1]
 print(min(triangle[-1]))
